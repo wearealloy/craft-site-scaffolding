3892fa4ce3cc4dbf3bd887461dba0545a5ea5f02
Anuar Reyes <anuar@hey.com>
2026-03-26 11:46:31 -0600

    Initial commit

---
+++ src/entrypoint.ts
@@ -0,0 +1,9 @@
+import "./css/global.css"
+import "./ts/app"
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log("HMR")
+  })
+}
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
+import { defineConfig } from "vite"
+
+const PORT = 5173
+const ORIGIN = `${process.env.DDEV_PRIMARY_URL}:${PORT}`
+
+export default defineConfig(({ command }) => ({
+  base: command === "serve" ? "" : "/dist/",
+  publirDir: path.resolve(__dirname, "src/public"),
+  build: {
+    manifest: true,
+    outDir: path.resolve(__dirname, "public_html/dist/"),
+    rollupOptions: {
+      input: {
+        entrypoint: path.resolve(__dirname, "src/entrypoint.ts"),
+      },
+    },
+    sourcemap: false,
+    emptyOutDir: true,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: PORT,
+    origin: ORIGIN,
+    strictPort: true,
+    cors: {
+      origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+    },
+  },
+  plugins: [tailwindcss()],
+}))
